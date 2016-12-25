4d39ae90a358663853e6c5af2826482972167f48
Marquavious Draggon <marq.draggon@gmail.com>
2016-12-25 15:43:37 -0500

    The start

---
+++ swipe_to_show_new_page/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swipe_to_show_new_page
+//
+//  Created by Marquavious on 12/25/16.
+//  Copyright © 2016 marqmakesapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
