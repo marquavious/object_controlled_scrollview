e089bdb10bf519bbbae530ea374a086ae36a760f
Marquavious Draggon <marq.draggon@gmail.com>
2016-12-25 19:16:58 -0500

    done...... MERRY CHRISTMAS🎄

--- swipe_to_show_new_page/ViewController.swift
+++ swipe_to_show_new_page/ViewController.swift
@@ -12,12 +12,19 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var scrollView: UIScrollView!
     var arrayOfSampleObjects = [
-        SampleObject(sampleText: "Scrolling"),
-        SampleObject(sampleText: "Page"),
-        SampleObject(sampleText: "Example"),
-        ]
+        SampleObject(sampleText: "Page 1"),
+        SampleObject(sampleText: "Page 2"),
+        SampleObject(sampleText: "Page 3"),
+        SampleObject(sampleText: "Page 4"),
+        SampleObject(sampleText: "Page 5"),
+        SampleObject(sampleText: "Page 6"),
+        SampleObject(sampleText: "Page 7"),
+        SampleObject(sampleText: "Page 8"),
+        SampleObject(sampleText: "Page 9"),
+        SampleObject(sampleText: "Page 10")
+    ]
     
-    var images = [UIView]()
+    var mainViews = [UIView]()
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,42 +34,45 @@ class ViewController: UIViewController {
         let scrollviewWidth = scrollView.frame.size.width
         let scrollviewHeight = scrollView.frame.size.height
         
-        //Loops through and add images to the images array
-        for x in 0...4{
+        for x in 0..<arrayOfSampleObjects.count{
             let textToShowInformation = UILabel()
             let viewToShowInformation = UIView()
             
             if x % 2 == 1{
-                viewToShowInformation.backgroundColor = .orange
+                viewToShowInformation.backgroundColor = .red
             } else {
                 viewToShowInformation.backgroundColor = .green
             }
-
-            images.append(viewToShowInformation)
+            
+            mainViews.append(viewToShowInformation)
             
             var newX: CGFloat = 0.0
             
             newX = scrollviewWidth * CGFloat(x)
             
-            contentWidth += newX
+            //come back to this
+            contentWidth += newX - scrollviewWidth/3
             
-            textToShowInformation.text = "IT IS LIT"
+            textToShowInformation.text = arrayOfSampleObjects[x].sampleText
             textToShowInformation.backgroundColor = .yellow
             textToShowInformation.translatesAutoresizingMaskIntoConstraints = false
             textToShowInformation.textAlignment = .center
             scrollView.addSubview(viewToShowInformation)
             viewToShowInformation.addSubview(textToShowInformation)
-      
+            
             
             viewToShowInformation.frame = CGRect(x: newX, y:0, width: scrollviewWidth, height: scrollviewHeight)
             
             textToShowInformation.centerXAnchor.constraint(equalTo: viewToShowInformation.centerXAnchor).isActive = true
             textToShowInformation.centerYAnchor.constraint(equalTo: viewToShowInformation.centerYAnchor).isActive = true
             textToShowInformation.heightAnchor.constraint(equalToConstant: 31).isActive = true
-//            textToShowInformation.widthAnchor.constraint(equalToConstant: 100).isActive = true
         }
+        scrollView.bounces = false
+        //        scrollView.contentSize = CGSize(width: CGFloat(Int(contentWidth)*arrayOfSampleObjects.count/2) - view.frame.width, height:scrollviewHeight)
         
-        scrollView.contentSize = CGSize(width: contentWidth, height:scrollviewHeight)
+        //        scrollView.contentSize = CGSize(width: CGFloat(contentWidth) - scrollView.frame.width * 1/3, height:scrollviewHeight)
+        
+        scrollView.contentSize = CGSize(width: scrollView.frame.size.width * CGFloat(arrayOfSampleObjects.count), height:scrollviewHeight)
         scrollView.clipsToBounds = false
     }
     
